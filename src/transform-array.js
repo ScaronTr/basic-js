4ad3663412157e87dd52350bbc7fb772418d6832
ScaronTr <forever2652453@gmail.com>
2022-04-28 19:46:41 +0300

    feat: add function transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  let resultArr = [];
+  outer: for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next') {
+      ++i;
+      continue outer;
+    }
+    if (arr[i] == '--discard-prev') {
+      if (i != 0 && arr[i - 2] != '--discard-next') resultArr.pop();
+      continue outer;
+    }
+    if (arr[i] == '--double-next') {
+      if (i != arr.length - 1) resultArr.push(arr[i + 1]);
+      continue outer;
+    }
+    if (arr[i] == '--double-prev') {
+      if (i != 0 && arr[i - 2] != '--discard-next') resultArr.push(arr[i - 1]);
+      continue outer;
+    }
+    resultArr.push(arr[i]);
+  }
+  return resultArr;
+
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
 }
 
 module.exports = {
