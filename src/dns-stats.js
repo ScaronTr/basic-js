5fe3464d2cdfded13e74ea034ae55ff79f2e1bce
ScaronTr <forever2652453@gmail.com>
2022-05-01 10:14:45 +0300

    feat: add func getDNSStats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let obj = {};
+  let key;
+
+  for (let item of domains) {
+    item = item.split('.');
+    item.reverse();
+    key = '';
+
+    for (let i = 0; i < item.length; i++) {
+      key += `.${item[i]}`;
+      if (!obj[key]) {
+        obj[key] = 1;
+      } else obj[key] += 1;
+    }
+  }
+  return obj;
 }
 
 module.exports = {
