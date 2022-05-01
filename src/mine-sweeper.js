280cc258acde7a7d2a61543860c65052dc391fa8
ScaronTr <forever2652453@gmail.com>
2022-05-01 15:07:53 +0300

    feat: implement func minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,80 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let countMines;
+  let resultField = [];
+
+  for (let row = 0; row < matrix.length; row++) {
+    resultField.push([]);
+    for (let col = 0; col < matrix[0].length; col++) {
+      resultField[row].push(0);
+    }
+  }
+
+  for (
+    let col = 0; col < matrix[0].length; col++) {
+    for (let row = 0; row < matrix.length; row++) {
+      countMines = 0;
+      if (row == 0 && col == 0) {
+          if (matrix[row][col + 1]) ++countMines;
+          if (matrix[row + 1][col]) ++countMines;
+          if (matrix[row + 1][col + 1]) ++countMines; 
+        } else if (row == matrix.length - 1 && col == 0) {
+          if (matrix[row - 1][col]) ++countMines;
+          if (matrix[row - 1][col + 1]) ++countMines;
+          if (matrix[row][col + 1]) ++countMines; 
+        } else if (row == 0 && col == matrix[0].length - 1) {
+          if (matrix[row][col - 1]) ++countMines;
+          if (matrix[row + 1][col - 1]) ++countMines;
+          if (matrix[row + 1][col]) ++countMines; 
+        } else if (row == matrix.length - 1 && col == matrix[0].length - 1) {
+          if (matrix[row - 1][col - 1]) ++countMines;
+          if (matrix[row - 1][col]) ++countMines;
+          if (matrix[row][col - 1]) ++countMines; 
+        }
+
+        else if (row == 0 && col > 0) {
+          if (matrix[row][col - 1]) ++countMines;
+          if (matrix[row + 1][col - 1]) ++countMines;
+          if (matrix[row][col + 1]) ++countMines;
+          if (matrix[row + 1][col]) ++countMines;
+          if (matrix[row + 1][col + 1]) ++countMines; 
+        } else if (row > 0 && col == 0) {
+          if (matrix[row - 1][col]) ++countMines;
+          if (matrix[row - 1][col + 1]) ++countMines;
+          if (matrix[row][col + 1]) ++countMines;
+          if (matrix[row + 1][col + 1]) ++countMines;
+          if (matrix[row + 1][col]) ++countMines;
+        } else if (row > 0 && col == matrix[0].length - 1) {
+          if (matrix[row - 1][col]) ++countMines;
+          if (matrix[row - 1][col - 1]) ++countMines;
+          if (matrix[row][col - 1]) ++countMines;
+          if (matrix[row + 1][col - 1]) ++countMines;
+          if (matrix[row + 1][col]) ++countMines; 
+        } else if (row == matrix.length - 1 && col > 0) {
+          if (matrix[row][col + 1]) ++countMines;
+          if (matrix[row - 1][col + 1]) ++countMines;
+          if (matrix[row - 1][col - 1]) ++countMines;
+          if (matrix[row - 1][col]) ++countMines;
+          if (matrix[row][col - 1]) ++countMines; 
+        } 
+        
+        else if (row > 0 && col > 0) {
+          if (matrix[row - 1][col - 1]) ++countMines;
+          if (matrix[row - 1][col]) ++countMines;
+          if (matrix[row - 1][col + 1]) ++countMines;
+          if (matrix[row][col - 1]) ++countMines;
+          if (matrix[row][col + 1]) ++countMines;
+          if (matrix[row + 1][col - 1]) ++countMines;
+          if (matrix[row + 1][col]) ++countMines;
+          if (matrix[row + 1][col + 1]) ++countMines;
+      } 
+
+      resultField[row][col] = countMines;
+    }
+  }
+  return resultField;
 }
 
 module.exports = {
