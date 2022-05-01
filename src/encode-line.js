d2ba49225da909f587525e676a074f6b63fb6fd5
ScaronTr <forever2652453@gmail.com>
2022-05-01 10:49:26 +0300

    feat: implement func encodeLine

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function encodeLine(str) {
+  let arrOfChar = [];
+  let arrOfValue = [];
+  let b = 0;
+  let result = [];
+
+  for(let i = 0; i < str.length; i++) {
+    if (str[i] != str[i - 1]) {
+      arrOfChar.push(str[i]);
+    }
+    if (str[i] == str[i + 1]) {
+      if (arrOfValue[b] === undefined) {
+        arrOfValue[b] = 1;
+      }
+      arrOfValue[b] += 1;
+    } else {
+      if (arrOfValue[b] === undefined) {
+        arrOfValue[b] = 1;
+      }
+      ++b;
+    }
+  }
+
+  for (let i = 0; i < arrOfChar.length; i++) {
+    if (arrOfValue[i] == 1) {
+      result.push(arrOfChar[i])
+    } else {
+      result.push(`${arrOfValue[i] + arrOfChar[i]}`)
+    }
+  }
+  return result.join('');
 }
 
 module.exports = {
