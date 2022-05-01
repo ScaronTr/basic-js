9b5ccabe8ffea176fc9aba3b2af47f7546eeb3d1
ScaronTr <forever2652453@gmail.com>
2022-05-01 13:10:24 +0300

    feat: implement func renameFiles

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let arr = [];
+  let slice;
+  let counterCopy = 1;
+  let counterCopyOfCopy = 1;
+
+  for (let i = 0; i < names.length; i++) {
+    if (i == 0) {
+      slice = [];
+      arr.push(names[0]);
+    } else {
+      slice = names.slice(0, i);
+      if (slice.includes(names[i]) && arr.includes(names[i])) {
+        arr.push(`${names[i]}(${counterCopy})`);
+        ++counterCopy;
+      }  else if (!slice.includes(names[i]) && arr.includes(names[i])) {
+        arr.push(`${names[i]}(${counterCopyOfCopy})`);
+        ++counterCopyOfCopy;
+      } else if (!slice.includes(names[i] && !arr.includes(names[i]))) {
+        arr.push(names[i]);
+      }
+    }
+  }
+  return arr;
 }
 
 module.exports = {
